6e159ac5ebee1849ab1970a4f98969d70c321edc
Alexander Martinez <jhonbrandonmartinezv@gmail.com>
2026-08-05 19:01:18 -0500

    Agregar licencia-uso-marca a TIPO_TRAMITE

--- server/src/routes/minutas.js
+++ server/src/routes/minutas.js
@@ -118,6 +118,7 @@ const TIPO_TRAMITE = {
   'endoso-titulo-valor':'Privado',
   'contrato-distribucion':'Privado',
   'acuerdo-confidencialidad-comercial':'Privado',
+  'licencia-uso-marca':'Privado',
   'pagare':'Privado','carta-instrucciones-pagare':'Privado',
   'compraventa-establecimiento-comercio':'Privado','arrendamiento-local-comercial':'Privado',
   'subarriendo-local-comercial':'Privado','agencia-comercial':'Privado',
